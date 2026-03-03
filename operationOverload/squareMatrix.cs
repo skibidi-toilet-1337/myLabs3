c4f24bd93e49c7c6c4cd982e554ff50ac63b034c
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-03-03 20:44:33 +0700

    myLab3 new changes №5

--- operationOverload/squareMatrix.cs
+++ operationOverload/squareMatrix.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace operationOverload {
-  public class squareMatrix {
+  public class squareMatrix : IComparable<squareMatrix> {
 
     private double[,] matrix;
     public int matrixSize;
+    public const int hashValue = 31;
 
     public squareMatrix(int size) {
 
@@ -16,15 +17,15 @@ namespace operationOverload {
         throw new ArgumentException("The matrix size must be greater than zero.");
       }
 
-        matrixSize = size;
-        matrix = new double[size, size];
+      matrixSize = size;
+      matrix = new double[size, size];
 
-        Random randomGen = new Random();
-        for (int row = 0; row < size; ++row) {
-          for (int col = 0; col < size; ++col) {
-            matrix[row,col] = randomGen.Next(-10, 10);
-          }
+      Random randomGen = new Random();
+      for (int row = 0; row < size; ++row) {
+        for (int col = 0; col < size; ++col) {
+          matrix[row, col] = randomGen.Next(-10, 10);
         }
+      }
     }
 
     public squareMatrix(double[,] newSqrtMatrix) {
@@ -39,7 +40,7 @@ namespace operationOverload {
       matrix = new double[matrixSize, matrixSize];
 
       Array.Copy(newSqrtMatrix, matrix, newSqrtMatrix.Length);
-     
+
     }
 
     public override string ToString() {
@@ -48,16 +49,47 @@ namespace operationOverload {
 
       for (int row = 0; row < matrixSize; ++row) {
         for (int col = 0; col < matrixSize; ++col) {
-          result += ((col == 0) ? (""): ("\t")) + matrix[row, col];
+          result += ((col == 0) ? ("") : ("\t")) + matrix[row, col];
         }
         result += "\n";
       }
       return result;
     }
 
-    //public override int CompareTo() {
+    public int CompareTo(squareMatrix other) {
+      throw new NotImplementedException();
+    }
 
-    //}
+    public override bool Equals(object obj) { 
+      squareMatrix otherMatrix = obj as squareMatrix;
+
+      if (this.matrixSize != otherMatrix.matrixSize) {
+        throw new ArgumentException("The size of the matrices must be the same.");
+      }
+
+      for (int row = 0; row < matrixSize; ++row) {
+        for (int col = 0; col < matrixSize; ++col) {
+
+          if (otherMatrix.matrix[row, col] != this.matrix[row, col]) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+
+    public override int GetHashCode() {
+
+      int hash = matrixSize;
+      foreach (double value in matrix) {
+        hash = hash * hashValue + value.GetHashCode();
+      }
+
+      return hash;
+
+
+    }
 
   }
 }
