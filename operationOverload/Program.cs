67108c4a441f1ec5e9231b70c56fb74351f1ff1e
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-03-05 15:07:48 +0700

    myLab3 new changes №15

--- operationOverload/Program.cs
+++ operationOverload/Program.cs
@@ -12,21 +12,23 @@ namespace operationOverload {
     static void showMenu() {
       Console.WriteLine("\nMenu:\n" +
         "----------------\n" +
-        "1: Set matrix A\n" +
-        "2: Set matrix B\n" +
-        "3: A + B\n" +
-        "4: A * B\n" +
-        "5: A > B\n" +
-        "6: A < B\n" +
-        "7: A <= B\n" +
-        "8: A >= B\n" +
-        "9: A == B\n" +
-        "10: A != B\n" +
-        "11: Det A\n" +
-        "12: Det B\n" +
-        "13: Inv A\n" +
-        "14: Inv B\n" +
-        "15: Show matrices\n" +
+        "1: Gen matrix A\n" +
+        "2: Gen matrix B\n" +
+        "3: Set matrix A\n" +
+        "4: Set matrix B\n" +
+        "5: Show matrices\n" +
+        "6: A + B\n" +
+        "7: A * B\n" +
+        "8: A > B\n" +
+        "9: A < B\n" +
+        "10: A <= B\n" +
+        "11: A >= B\n" +
+        "12: A == B\n" +
+        "13: A != B\n" +
+        "14: Det A\n" +
+        "15: Det B\n" +
+        "16: Inv A\n" +
+        "17: Inv B\n" +
         "----------------\n" +
         "0: Exit\n");
       Console.Write("Select option: ");
@@ -67,8 +69,12 @@ namespace operationOverload {
     static void Main(string[] args) {
 
       string option;
-      matA = new squareMatrix(1, false);
-      matB = new squareMatrix(1, false);
+      int sizeA = 1;
+      int sizeB = 1;
+
+      matA = new squareMatrix(sizeA, false);
+      matB = new squareMatrix(sizeB, false);
+
       while (true) {
         showMenu();
         option = Console.ReadLine();
@@ -80,70 +86,102 @@ namespace operationOverload {
           case "0":
             return;
           
-          //Set matrix A
+          //Gen matrix A
           case "1":
+            Console.Write("Enter size of matrix: ");
+            if (int.TryParse(Console.ReadLine(), out int inSizeA)) {
+              sizeA = inSizeA;
+            } else {
+              throw new matricesMismatchSize("Wrong value. Must be integer.");
+            }
+            matA = new squareMatrix(sizeA, true);
+            break;
+
+          //Gen matrix B
+          case "2":
+            Console.Write("Enter size of matrix: ");
+            if (int.TryParse(Console.ReadLine(), out int inSizeB)) {
+              sizeB = inSizeB;
+            } else {
+              throw new matricesMismatchSize("Wrong value. Must be integer.");
+            }
+            matB = new squareMatrix(sizeB, true);
+            break;
+
+          //Set matrix A
+          case "3":
             matA.inputMatrix();
             break;
 
           //Set matrix B
-          case "2":
+          case "4":
             matB.inputMatrix();
             break;
 
+          //Show matrices
+          case "5":
+            Console.WriteLine($"Matrix A:\n{matA}\n");
+            Console.WriteLine($"Matrix B:\n{matB}\n");
+            break;
+
           //A + B
-          case "3":
+          case "6":
             Console.WriteLine($"A + B:\n{matA + matB}");
             break;
 
           //A * B
-          case "4":
+          case "7":
             Console.WriteLine($"A * B:\n{matA * matB}");
             break;
 
           //A > B
-          case "5":
+          case "8":
+            Console.WriteLine($"A > B:\n{matA > matB}");
             break;
 
           //A < B
-          case "6":
+          case "9":
+            Console.WriteLine($"A < B:\n{matA < matB}");
             break;
 
           //A <= B
-          case "7":
+          case "10":
+            Console.WriteLine($"A <= B:\n{matA <= matB}");
             break;
 
           //A >= B
-          case "8":
+          case "11":
+            Console.WriteLine($"A >= B:\n{matA >= matB}");
             break;
 
           //A == B
-          case "9":
+          case "12":
+            Console.WriteLine($"A == B:\n{matA == matB}");
             break;
 
           //A != B
-          case "10":
+          case "13":
+            Console.WriteLine($"A != B:\n{matA != matB}");
             break;
 
           //Det A
-          case "11":
+          case "14":
+            Console.WriteLine($"Det A:\n{matA.determinant()}");
             break;
 
           //Det B
-          case "12":
+          case "15":
+            Console.WriteLine($"Det B:\n{matB.determinant()}");
             break;
 
           //Inv A
-          case "13":
+          case "16":
+            Console.WriteLine($"Inv A:\n{matA.inverse()}");
             break;
 
           //Inv B
-          case "14":
-            break;
-
-          //Show matrices
-          case "15":
-            Console.WriteLine($"Matrix A:\n{matA}\n");
-            Console.WriteLine($"Matrix B:\n{matB}\n");
+          case "17":
+            Console.WriteLine($"Inv B:\n{matB.inverse()}");
             break;
 
         }
