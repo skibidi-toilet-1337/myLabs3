9dcd86f8d4c3449b9815777b228aada0b6ca3db2
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-03-17 21:13:59 +0700

    SIO new changes №2

--- StandartInputOutput2/Program.cs
+++ StandartInputOutput2/Program.cs
@@ -4,21 +4,34 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace StandartInputOutput2 {
   internal class Program {
     static void Main(string[] args) {
 
-      FileStream fs = new FileStream("D:\\FullNameSerialize.bin", FileMode.OpenOrCreate, FileAccess.Write);
-      TextFile fnc = new TextFile("Ivan", "Ivanov", "Ivanovich");
-      fnc.Print();
-      fnc.Print();
-      fnc.Serialize(fs);
-      fnc = new TextFile("Petr", "Petrov", "Petrovich");
-      fnc.Print();
-      fs = new FileStream("D:\\FullNameSerialize.bin", FileMode.OpenOrCreate, FileAccess.Read);
-      fnc.Deserialize(fs);
-      fnc.Print();
+      //FileStream fs = new FileStream("D:\\newText.bin", FileMode.OpenOrCreate, FileAccess.Write);
+      string path = "D:\\newText";
+      TextFile tf = new TextFile("Lorem Ipsum");
+
+      using (FileStream fs = new FileStream(path + ".bin", FileMode.OpenOrCreate, FileAccess.ReadWrite)) {
+        tf.SerializeBinary(fs);
+        Console.WriteLine("Object has been binary serialized");
+        fs.Seek(0, SeekOrigin.Begin);
+
+        tf.DeserializeBinary(fs);
+        Console.WriteLine("Object has been binary deserialized");
+      }
+
+      using (FileStream fs = new FileStream(path + ".xml", FileMode.OpenOrCreate, FileAccess.ReadWrite)) {
+        tf.SerializeXML(fs);
+        Console.WriteLine("Object has been XML serialized");
+        fs.Seek(0, SeekOrigin.Begin);
+
+        tf.DeserializeXML(fs);
+        Console.WriteLine("Object has been XML deserialized");
+
+      }
 
     }
   }
--- StandartInputOutput2/TextFile.cs
+++ StandartInputOutput2/TextFile.cs
@@ -3,36 +3,53 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace StandartInputOutput2 {
 
   [Serializable]
-  internal class TextFile {
+  public class TextFile {
 
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string MiddleName { get; set; }
-    public TextFile(string name, string surname, string middle) {
-      Name = name; Surname = surname; MiddleName = middle;
+    public string inputText { get; set; }
+    public TextFile() {
+
     }
-    public void Serialize(FileStream fs) {
+
+    public TextFile(string text) {
+      inputText = text;
+    }
+    public void SerializeBinary(FileStream fs) {
       BinaryFormatter bf = new BinaryFormatter();
       bf.Serialize(fs, this);
-      fs.Flush();
-      fs.Close();
+      //fs.Flush();
+      //fs.Close();
     }
-    public void Deserialize(FileStream fs) {
+    public void DeserializeBinary(FileStream fs) {
       BinaryFormatter bf = new BinaryFormatter();
       TextFile deserialized = (TextFile)bf.Deserialize(fs);
-      Name = deserialized.Name;
-      Surname = deserialized.Surname;
-      MiddleName = deserialized.MiddleName;
-      fs.Close();
+      inputText = deserialized.inputText;
+      //fs.Close();
     }
+
+    public void SerializeXML(FileStream fs) {
+      XmlSerializer xmlS = new XmlSerializer(typeof(TextFile));
+      xmlS.Serialize(fs, this);
+      //fs.Flush();
+      //fs.Close();
+    }
+
+    public void DeserializeXML(FileStream fs) {
+      XmlSerializer xmlS = new XmlSerializer(typeof(TextFile));
+      TextFile deserialized = (TextFile)xmlS.Deserialize(fs);
+      inputText = deserialized.inputText;
+      //fs.Close();
+    }
+
     public void Print() {
-      Console.WriteLine("Name={0} Surname={1} Middle={2}", Name, Surname, MiddleName);
+      Console.WriteLine(inputText);
     }
 
   }
