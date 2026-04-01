73a670dd79eeb258a893ac3ba4cd10dfe2134383
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-04-01 22:27:46 +0700

    Добавлен поиск слов через регулярное выражение

--- StringsAndCollections/Program.cs
+++ StringsAndCollections/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -8,6 +9,35 @@ using System.Threading.Tasks;
 namespace StringsAndCollections {
   internal class Program {
     static void Main(string[] args) {
+      string path;
+      string wordFind = "[а-яёЁА-Яa-zA-Z]+";
+      Regex regex = new Regex(wordFind);
+      
+      Console.Write(@"Enter directory path (D:\test): ");
+      //path = Console.ReadLine();
+      path = "D:\\Test\\";
+
+      var fileNames = Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
+
+      var wrongWordsDict = GetWrongDictionary(); 
+
+      Console.WriteLine("\nFounded files: ");
+      foreach (var fileName in fileNames) {
+        string fileContent = File.ReadAllText(fileName);
+        Console.WriteLine(fileName);
+
+        MatchCollection matches = regex.Matches(fileContent);
+        Console.WriteLine();
+        foreach (Match match in matches) {
+          Console.Write(" "+match);
+        }
+
+        foreach (var wrongWord in wrongWordsDict) {
+          
+        }
+        Console.WriteLine();
+      }
+
 
     }
 
